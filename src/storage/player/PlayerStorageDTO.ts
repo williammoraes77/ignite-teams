d81e2063f3c5a3b36186f826098e8f1cb9e8a6f4
William Moraes <willmoraes77@gmail.com>
2023-01-10 13:50:46 -0300

    fix bgheads

--- src/storage/player/PlayerStorageDTO.ts
+++ src/storage/player/PlayerStorageDTO.ts
@@ -2,20 +2,123 @@ export type PlayerStorageDTO = {
   name: string;
   team: string;
   avatar?: {
-    acessory: string;
-    bgColor: string;
-    body: string;
-    cloth: string;
-    clothColor: string;
-    eyeBrow: string;
-    eye: string;
-    facialHair: string;
-    hair: string;
-    hairColor: string;
-    hat: string;
-    hatColor: string;
-    lipColor: string;
-    mouth: string;
-    skinTone: string;
+    acessory:
+      | "roundGlasses"
+      | "tinyGlasses"
+      | "shades"
+      | "faceMask"
+      | "hoopEarrings"
+      | "none"
+      | undefined;
+    bgColor:
+      | "blue"
+      | "green"
+      | "red"
+      | "orange"
+      | "yellow"
+      | "turqoise"
+      | "pink"
+      | "purple"
+      | undefined;
+    body: "chest" | "breasts" | undefined;
+    cloth:
+      | "naked"
+      | "shirt"
+      | "dressShirt"
+      | "vneck"
+      | "tankTop"
+      | "dress"
+      | "denimJacket"
+      | "hoodie"
+      | "chequeredShirt"
+      | "chequeredShirtDark"
+      | undefined;
+    clothColor: "blue" | "green" | "red" | "white" | "black" | undefined;
+    eyeBrow:
+      | "raised"
+      | "leftLowered"
+      | "serious"
+      | "angry"
+      | "concerned"
+      | undefined;
+    eye:
+      | "normal"
+      | "leftTwitch"
+      | "happy"
+      | "content"
+      | "squint"
+      | "simple"
+      | "dizzy"
+      | "wink"
+      | "hearts"
+      | "crazy"
+      | "cute"
+      | "dollars"
+      | "stars"
+      | "cyborg"
+      | "simplePatch"
+      | "piratePatch"
+      | undefined;
+    facialHair:
+      | "none"
+      | "stubble"
+      | "mediumBeard"
+      | "goatee"
+      | "none2"
+      | "none3"
+      | undefined;
+    hair:
+      | "none"
+      | "short"
+      | "long"
+      | "bun"
+      | "pixie"
+      | "balding"
+      | "buzz"
+      | "afro"
+      | "bob"
+      | "mohawk"
+      | undefined;
+    hairColor:
+      | "blue"
+      | "white"
+      | "blonde"
+      | "orange"
+      | "pink"
+      | "black"
+      | "brown"
+      | undefined;
+    hat:
+      | "none"
+      | "none2"
+      | "none3"
+      | "beanie"
+      | "turban"
+      | "party"
+      | "hijab"
+      | "none4"
+      | "none5"
+      | undefined;
+    hatColor: "blue" | "green" | "red" | "white" | "black" | undefined;
+    lipColor: "green" | "red" | "turqoise" | "pink" | "purple" | undefined;
+    mouth:
+      | "serious"
+      | "lips"
+      | "grin"
+      | "sad"
+      | "openSmile"
+      | "open"
+      | "tongue"
+      | "piercedTongue"
+      | "vomitingRainbow"
+      | undefined;
+    skinTone:
+      | "red"
+      | "yellow"
+      | "black"
+      | "brown"
+      | "light"
+      | "dark"
+      | undefined;
   };
 };
